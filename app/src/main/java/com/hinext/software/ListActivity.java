a327fa21fea742a5202b7d675a0007369aa8c4c7
Nazmul <nahasan_ael@outlook.com>
2022-12-07 16:17:04 +0600

    add text and logo in login page

--- app/src/main/java/com/hinext/software/ListActivity.java
+++ app/src/main/java/com/hinext/software/ListActivity.java
@@ -3,10 +3,13 @@ package com.hinext.software;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.hinext.software.ModelClass.Datum;
@@ -23,6 +26,9 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        //setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
         Button login = findViewById(R.id.button);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +37,26 @@ public class ListActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menu_item_logout:
+                finish();
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
